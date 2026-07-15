bfe3a4abc59a78d80fffe98da65a746d5f8b2927
Prashant Dubey <Prashantdubey02feb2019@gmail.com>
2026-07-15 10:34:47 +0530

    Add solution of the Removing outermost parenthesis from a valid parenthesis string

---
+++ src/strings/basicandeasystringproblems/OutermostParenthesis.java
@@ -0,0 +1,78 @@
+package strings.basicandeasystringproblems;
+
+/*
+
+Platform     : GeeksforGeeks/LeetCode(4)
+Problem Link : https://leetcode.com/problems/remove-outermost-parentheses/description/
+Problem      : Median Of 2 Sorted Arrays
+Topic        : String 
+Category     : Basic and Easy problems
+Difficulty   : Easy
+
+Date last solved : 15 July 2026
+
+*/
+
+/*----------------------------------------------------------
+BRUTE FORCE
+-----------------------------------------------------------
+
+Approach         : Brute Force
+Time Complexity  : O(n²)
+Space Complexity : O(n)
+
+*/
+
+class Solution {
+    public String removeOuter(String s) {
+        int count = 0;
+        String ans = "";
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == ')') {
+                count--;
+            }
+            if(count != 0) {
+                ans += s.charAt(i);
+            }
+            if(s.charAt(i) == '(') {
+                count++;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+/*----------------------------------------------------------
+OPTIMAL
+-----------------------------------------------------------
+
+Approach         : Optimal(Binary-Search)
+Time Complexity  : O(n)
+Space Complexity : O(n)
+
+*/
+
+
+//  Code
+
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ')') {
+                count--;
+            }
+            if(count != 0) {
+                ans.append(s.charAt(i));
+            }
+            if(s.charAt(i) == '(') {
+                count++;
+            }
+        }
+        return ans.toString();
+    }
+}
