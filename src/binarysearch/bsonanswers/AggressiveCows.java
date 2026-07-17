8039497c9d68b5fdf14a02b332b6afded250b52f
Prashant Dubey <Prashantdubey02feb2019@gmail.com>
2026-07-18 00:17:12 +0530

    Added optimal Solution of the Aggressive Cows problem

--- src/binarysearch/bsonanswers/AggressiveCows.java
+++ src/binarysearch/bsonanswers/AggressiveCows.java
@@ -52,3 +52,49 @@ class Solution {
     }
 }
 
+
+/*----------------------------------------------------------
+OPTIMAL
+-----------------------------------------------------------
+
+Approach         : Optimal(Binary-Search)
+Time Complexity  : O(nlog n + nlog d)
+Space Complexity : O(n)
+
+*/
+
+
+//  Code
+
+class Solution {
+    public int aggressiveCows(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = -1;
+        int start = 1;
+        int end = nums[nums.length-1] - nums[0];
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            int cntCows = countCows(nums, mid);
+            if(cntCows >= k) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+    int countCows(int [] nums, int dist) {
+        int totalCows = 1;
+        int last = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] - last >= dist) {
+                totalCows++;
+                last = nums[i];
+            }
+        }
+        return totalCows;
+    }
+}
+
