339643999bdf02fb2748ce5e275ad139595c84bc
Prashant Dubey <Prashantdubey02feb2019@gmail.com>
2026-08-02 15:48:01 +0530

    Added solution for the longest subarray with sum k

--- src/arrays/easyproblems/LongestSubArray.java
+++ src/arrays/easyproblems/LongestSubArray.java
@@ -31,3 +31,80 @@ class Solution {
         return ans;
     }
 }
+
+
+/*
+
+Approach         : Better
+Time Complexity  : N log M + O(n/2 + 1)
+Space Complexity : O(n/2 + 1)
+
+*/
+
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            sum += nums[i];
+
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+
+            if (map.containsKey(sum - k)) {
+                int len = i - map.get(sum - k);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
+
+
+/*
+
+Approach         : Optimal
+Time Complexity  : O(2n)
+Space Complexity : O(1)
+
+Note-> This solution is correct for non-negatives
+
+*/
+
+
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+
+        int left = 0; 
+        int right  = 0;
+        int sum = nums[0];
+        int maxLen = 0;
+        int n = nums.length;
+        
+        while(right < n) {
+            while(left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            if(sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+            if(right < n) {
+                sum += nums[right];
+            }
+        }
+        return maxLen;
+    }
+}
