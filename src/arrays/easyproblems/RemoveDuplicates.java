d34e1cd1495407ad7b82bc24c3302a35d8e6fe59
Prashant Dubey <Prashantdubey02feb2019@gmail.com>
2026-08-06 10:20:34 +0530

    Update RemoveDuplicates.java

--- src/arrays/easyproblems/RemoveDuplicates.java
+++ src/arrays/easyproblems/RemoveDuplicates.java
@@ -59,6 +59,8 @@ class Solution {
 class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 0; 
+        if(nums.length == 0)
+            return 0;
         nums[j++] = nums[0];
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] == nums[i-1]) {
